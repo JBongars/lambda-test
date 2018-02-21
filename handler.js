dd7aa692a76d5fae2f28c4e73e4306e66fc07036
Julien Bongars <julienbongars23@gmail.com>
2018-02-21 13:00:46 +0800

    first commit

--- handler.js
+++ handler.js
@@ -1,16 +1,23 @@
 'use strict';
 
+const aws = require('aws-sdk');
+const async = require('async');
+
+var s3 = new aws.S3();
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Service has been deployed successfully! v0.0.1',
+            input: event,
+            context: context
+        }),
+    };
+    callback(null, response);
 };
+
+module.exports.uploadImage = (event, context, callback) => {
+    console.log('resizing image!');
+    //callback(null, 'touched!');
+}
